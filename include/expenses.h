48d8e74e4076478af69b7f203a3d8b9d3490cbfe
EKTA BHARDWAJ <ebhardwaj743@gmail.com>
2025-11-25 20:14:54 +0530

    Add expenses.h header file with expense functions

---
+++ include/expenses.h
@@ -0,0 +1,15 @@
+#ifndef EXPENSES_H
+#define EXPENSES_H
+
+#include "utils.h"
+
+void loadExpenses(void);
+void saveExpenses(void);
+
+void expenseAdd(void);
+void expenseListShow(void);
+void expenseEdit(void);
+void expenseDelete(void);
+void expenseAlerts(void);
+
+#endif
