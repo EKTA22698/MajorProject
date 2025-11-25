611c5b40c86177539e9ff8b9e2906b3b1571be0b
EKTA BHARDWAJ <ebhardwaj743@gmail.com>
2025-11-25 20:14:20 +0530

    Add pantry.h header file with pantry functions

---
+++ include/pantry.h
@@ -0,0 +1,15 @@
+#ifndef PANTRY_H
+#define PANTRY_H
+
+#include "utils.h"
+
+void loadPantry(void);
+void savePantry(void);
+
+void pantryAdd(void);
+void pantryListShow(void);
+void pantryEdit(void);
+void pantryDelete(void);
+void pantryAlerts(void);
+
+#endif
