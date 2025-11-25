ec6b7e05075923198e2f33a4556b31499163e771
EKTA BHARDWAJ <ebhardwaj743@gmail.com>
2025-11-25 20:15:48 +0530

    Add movies.h header file with movie functions

---
+++ include/movies.h
@@ -0,0 +1,15 @@
+#ifndef MOVIES_H
+#define MOVIES_H
+
+#include "utils.h"
+
+void loadMovies(void);
+void saveMovies(void);
+
+void movieAdd(void);
+void movieListShow(void);
+void movieEdit(void);
+void movieDelete(void);
+void movieAlerts(void);
+
+#endif
