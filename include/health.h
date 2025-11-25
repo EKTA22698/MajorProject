22e223b9e8939c509fd6a6a235e24f735f64c08d
EKTA BHARDWAJ <ebhardwaj743@gmail.com>
2025-11-25 20:15:23 +0530

    Add health.h header file with health functions

---
+++ include/health.h
@@ -0,0 +1,15 @@
+#ifndef HEALTH_H
+#define HEALTH_H
+
+#include "utils.h"
+
+void loadHealth(void);
+void saveHealth(void);
+
+void healthAdd(void);
+void healthListShow(void);
+void healthEdit(void);
+void healthDelete(void);
+void healthAlerts(void);
+
+#endif
